365e1d9df16224551def2bc0ac3c8f5e1333b3e1
Piyush Jain <piyushmbm45@gmail.com>
2023-06-29 19:36:18 +0530

    profile img updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,11 @@
 import resume from '../assets/pdf/piyush_full_Stack_developer.pdf';
+import img from '../assets/png/profile_photo.png';
 
 export const headerData = {
   name: 'Piyush Jain',
   title: 'Full Stack Web Developer',
   description:
     "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-  image: '/favicon.png',
+  image: img,
   resumePdf: resume,
 };
