4f4aad25be0c2dd05717b2e62cc59f56ae32f07d
PIYUSH JAIN <piyushjain@PIYUSHs-MacBook-Air.local>
2023-11-23 08:43:41 +0530

    company added

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,7 +3,14 @@ export const experienceData = [
     id: 1,
     company: 'Arnowa - https://arnowa.com',
     jobtitle: 'Full Stack Web Developer',
-    startYear: '2021',
+    startYear: 'Oct-2021',
+    endYear: 'Sep-2023',
+  },
+  {
+    id: 2,
+    company: 'Nordem - https://nordemtech.com/',
+    jobtitle: 'Full Stack Developer',
+    startYear: 'Oct-2023',
     endYear: 'Present',
   },
 ];
